8288cff0a51782fce4c0e3e05e0df5c26e67b5b2
Vincent Cadoret <vincent.cadoret@cpe.fr>
2021-10-06 14:14:34 +0200

    06/10/2021 11:59:00 > avancement fonc1

--- cmake-build-debug/function.h
+++ cmake-build-debug/function.h
@@ -5,12 +5,37 @@
 #ifndef BENCHME_FUNCTION_H
 #define BENCHME_FUNCTION_H
 
+#include <assert.h>
+#include <time.h>
 
-void triSelection(float uneListe[], int uneTaille){
-    float temp = 0;
-    for (int i = 0; i < uneTaille; ++i) {
-        if(temp < uneListe[i]){
-            temp = uneListe[i];
+
+void *triSelection(float unTableau[], int uneTaille){
+    float temp;
+    int index;
+    for (int i = 0; i < (uneTaille - 1); i++){
+        index = i;
+        for (int j = i + 1; j < uneTaille; j++){
+            if (unTableau[index] > unTableau[j]){
+                index = j;
+            }
+        }
+        if (index != i){
+            temp = unTableau[i];
+            unTableau[i] = unTableau[index];
+            unTableau[index] = temp;
+        }
+    }
+}
+void *triInsertion(float unTableau[], int uneTaille){
+    int j = 0;
+    float temp;
+    for (int i = 1 ; i <= uneTaille - 1; i++) {
+        j = i;
+        while (j > 0 && unTableau[j - 1] > unTableau[j]) {
+            temp = unTableau[j];
+            unTableau[j] = unTableau[j - 1];
+            unTableau[j - 1] = temp;
+            j--;
         }
     }
 }
--- main.c
+++ main.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "cmake-build-debug/function.h"
 
 int main() {
     /* Debut du BenchMe */
     printf("Debut du TP 002 !\n");
+    float tab[10] = { 3, 2, 7, 10, 5, 22, 1, 27, 25, 30};
 
-    printf("Fonction 1 :");
-    int tableau[15] = {0, 0, 0, 0};
+    /* Fonction n1 */
+    printf("Fonction 1 : ");
+    triSelection(tab, 10);
+    for (int i = 0; i < 10 ; i++){
+        printf("%.2f ", tab[i]);
+    }
 
-    printf("Fin du TP 002 !\n");
+    /* Fonction n2 */
+    printf("\nFonction 2 : ");
+    triInsertion(tab, 10);
+    for (int i = 0; i < 10 ; i++){
+        printf("%.2f ", tab[i]);
+    }
+
+
+    printf("\nFin du TP 002 !");
     return 0;
     /*Fin du BenchMe */
 }
