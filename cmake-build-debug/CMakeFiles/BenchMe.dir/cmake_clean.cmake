b3deb73ad298cb9d5f48f64b11219d735c235d1c
Vincent Cadoret <vincent.cadoret@cpe.fr>
2021-10-06 11:18:11 +0200

    Ajout function.h > 06/10/2021 11:17:00

---
+++ cmake-build-debug/CMakeFiles/BenchMe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BenchMe.exe"
+  "BenchMe.exe.manifest"
+  "BenchMe.pdb"
+  "CMakeFiles/BenchMe.dir/main.c.obj"
+  "libBenchMe.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BenchMe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/function.h
@@ -0,0 +1,15 @@
+//
+// Created by v.cadoret on 06/10/2021.
+//
+
+#ifndef BENCHME_FUNCTION_H
+#define BENCHME_FUNCTION_H
+
+
+
+void triSelection(int uneListe[2]){
+
+}
+
+
+#endif //BENCHME_FUNCTION_H
