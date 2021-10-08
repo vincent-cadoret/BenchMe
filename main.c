105fc408322cf8ee0a143b030eed7e09a72debbb
Vincent Cadoret <vincent.cadoret@cpe.fr>
2021-10-08 11:55:51 +0200

    08/10/2021 10:50 > mise à jour placement des fichiers

--- main.c
+++ main.c
@@ -4,57 +4,50 @@
 #include "function.h"
 
 int main(int argc, char *argv[]) {
-    /* Debut du BenchMe */
+    /* Début du BenchMe */
     printf("Debut du TP 002 !\n\n");
 
-    /*Génération du tableau*/
+    /* Génération du tableau */
 
-    srand(3);
-    const int tab_max = (int)pow(10, 2);
-    float *unTableau = (float*) malloc(sizeof(float) * tab_max);
-    float uneTaille = (float)10000000.0;
+    /* A deplacer dans test */
 
-    for (int i = 0; i < tab_max; i++) {
-        unTableau[i] = ((float)rand()/(float)(RAND_MAX)) * uneTaille;
+    float uneTaille = (float)10000000.0; // Définition taille des valeurs.
+    for (int i = 1; i < 4; ++i) { // 3 graines pour avoir 3 jeux complets de valeur.
+        srand(i);
+        for (int j = 2; j < 6; ++j) { // 4 types de longueur de tableau : 10^2,10^3,10^4,10^5.
+            int tab_max = (int)pow(10, j); // valeur du tableau
+            float *unTableau = (float*) malloc(sizeof(float) * tab_max); // Définition du tableau.
+            for (int l = 0; l < tab_max; l++) { // Remplissage des valeurs.
+                unTableau[l] = ((float)rand()/(float)(RAND_MAX)) * uneTaille; // Ajout d'un nombre aléatoire en position i
+            }
+            double temps = 0;
+            // Fonction n1
+            clock_t f11 = clock();
+            triSelection(unTableau, tab_max);
+            clock_t f12 = clock(); /* Variable de calcul de temps */
+            temps = tempsTotalsFonctions(f11,f12);
+
+            // Fonction n2
+            clock_t f21 = clock();
+            triInsertion(unTableau, tab_max);
+            clock_t f22 = clock(); /* Variable de calcul de temps */
+            temps = tempsTotalsFonctions(f21,f22);
+
+            // Fonction n3
+            clock_t f31 = clock();
+            triBulle(unTableau, tab_max);
+            clock_t f32 = clock(); /* Variable de calcul de temps */
+            temps = tempsTotalsFonctions(f31,f32);
+
+            // Fonction n4
+            clock_t f41 = clock();
+            triTas(unTableau, tab_max);
+            clock_t f42 = clock(); /* Variable de calcul de temps */
+            temps = tempsTotalsFonctions(f41,f42);
+        }
     }
+    /*fin test*/
 
-    /* Fonction n1 */
-
-    printf("\nFonction 1 : \n");
-    clock_t f11 = clock();
-    triSelection(unTableau, tab_max);
-    printArray(unTableau, tab_max);
-    clock_t f12 = clock(); /* Variable de calcul de temps */
-    printf("Temps : %.3f", tempsTotalsFonctions(f11,f12));
-
-    /* Fonction n2 */
-
-    printf("\nFonction 2 : \n");
-    clock_t f21 = clock();
-    triInsertion(unTableau, tab_max);
-    printArray(unTableau, tab_max);
-    clock_t f22 = clock(); /* Variable de calcul de temps */
-    printf("Temps : %.3f", tempsTotalsFonctions(f21,f22));
-
-    /* Fonction n3*/
-
-    printf("\nFonction 3 : \n");
-    clock_t f31 = clock();
-    triBulle(unTableau, tab_max);
-    printArray(unTableau, tab_max);
-    clock_t f32 = clock(); /* Variable de calcul de temps */
-    printf("Temps : %.3f", tempsTotalsFonctions(f31,f32));
-
-    /* Fonction n4 */
-
-    printf("\nFonction 4 : \n");
-    clock_t f41 = clock();
-    triTas(unTableau, tab_max);
-    printArray(unTableau, tab_max);
-    clock_t f42 = clock(); /* Variable de calcul de temps */
-    printf("Temps : %.3f", tempsTotalsFonctions(f41,f42));
-
-    printf("\nNom du fichier : %s\n", argv[1]);
     writeResult(argv[1]);
 
     printf("\nFin du TP 002 !");
